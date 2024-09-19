ed80de1da53e30851486a0a60d92d51e092abe19
erod164 <erod.71@outlook.com>
2024-09-19 13:02:49 -0700

    added input for n and number of tasks

--- PA_1/Multitasking/multitasking.c
+++ PA_1/Multitasking/multitasking.c
@@ -2,16 +2,40 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * Structure to hold thread partial sum data
+ */
+typedef struct
+{
+    unsigned long long int range_start;
+    unsigned long long int range_end;
+    long double partial_sum;
+} PartialSumData;
+
 int main(int argc, char const *argv[])
 {
-    pid_t pids[3];
-    int i;
+    unsigned long int n;
+    unsigned int tasks_size;
+    pid_t pids;
 
-    for (i = 0; i < 3; i++)
+    printf("Enter N: ");
+    scanf("%ld", &n);
+    printf("Enter task size: ");
+    scanf("%d", &tasks_size);
+
+    PartialSumData task_data[tasks_size];
+    unsigned long int range = n / tasks_size;
+    for (int i = 0; i < tasks_size; i++)
     {
-        pids[i] = fork();
+        task_data[i].range_start = i * range;
+        task_data[i].range_end = i + 1 * range;
+        task_data[i].partial_sum = 0;
+        pids = fork();
+
+        if (pids == 0)
+        {
+        }
     }
-    printf("Hello\n");
 
     // You can then determine uniquely identify the all available processes as a combination of pids array
 }
