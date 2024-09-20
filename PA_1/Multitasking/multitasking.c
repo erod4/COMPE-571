914bfbda900d02a1f34c2b57ddba9af0036e17c7
erod164 <erod.71@outlook.com>
2024-09-20 13:37:04 -0700

    provided comments to program for better understanding

--- PA_1/Multitasking/multitasking.c
+++ PA_1/Multitasking/multitasking.c
@@ -11,54 +11,59 @@ int main(int argc, char const *argv[])
     int fd[2];
     time_t start, end;
 
+    // get input from user
     printf("Enter N: ");
     scanf("%ld", &n);
     printf("Enter task size: ");
     scanf("%d", &tasks_size);
 
+    // calculate range size for each task
     unsigned long int range = n / tasks_size;
     pid_t pids[tasks_size];
 
+    // create a pipe for tasks to communicate
     pipe(fd);
-    start = time(NULL); // Start Timer
 
+    start = time(NULL); // Start Timer
     for (int i = 0; i < tasks_size; i++)
     {
-        pids[i] = fork();
 
-        if (pids[i] == 0)
+        pids[i] = fork(); //> create a fork for m task size
+
+        if (pids[i] == 0) //> check if its a child task
         {
+
+            // calcuate ith task start, end, and init its partial sum
             long int start = i * range;
             long int end = (i + 1) * range;
             long double partial_sum = 0.0;
 
-            for (unsigned long int j = start; j < end; j++)
+            for (unsigned long int j = start; j < end; j++) //> perform summation for ith task
             {
                 partial_sum += j;
             }
-            // printf("Start %ld\n", start);
-            // printf("End %ld\n", end);
-            // printf("Child Partial Sum: %Lf\n", partial_sum);
-            close(fd[0]);
-            write(fd[1], &partial_sum, sizeof(partial_sum));
-            exit(0);
+
+            close(fd[0]);                                    //> close read size of pipe
+            write(fd[1], &partial_sum, sizeof(partial_sum)); //> write ith tasks partial sum to the pipe
+            exit(0);                                         //> terminate child task to prevent child task from creating a new task
         }
     }
 
     for (int i = 0; i < tasks_size; i++)
     {
-        waitpid(pids[i], NULL, 0);
+        waitpid(pids[i], NULL, 0); //> parent task wait for all child tasks to complete
     }
+
+    // loop through and collect partial sums from  child tasks
     for (int i = 0; i < tasks_size; i++)
     {
         long double partial_sum = 0.0;
-        close(fd[1]);
-        read(fd[0], &partial_sum, sizeof(partial_sum));
-        // printf("Partial Sum Sent To Parent: %Lf\n", partial_sum);
-        sum += partial_sum;
+        close(fd[1]);                                   //> close write side of pipe
+        read(fd[0], &partial_sum, sizeof(partial_sum)); //> read from pipe and store into partial_sum temp var
+        sum += partial_sum;                             //> increment sum with each childs partial sum
     }
 
-    end = time(NULL);                             // End Timer
-    double elapsed_time_s = difftime(end, start); // Time difference in seconds
+    end = time(NULL);                             //> End Timer
+    double elapsed_time_s = difftime(end, start); //> Time difference in seconds
     printf("Sum: %Lf, Ellapsed Time: %f seconds\n", sum, elapsed_time_s);
 }
